569ff495a32dae93c155bbddae34cd92f4b6b7af
luigi112 <riosmunar_luigi@hotmail.com>
2022-06-14 17:16:57 -0500

    version1

---
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -0,0 +1,15 @@
+
+package cv_ejercicio;
+
+/**
+ *
+ * @author riosm
+ */
+public class Cv_ejercicio {
+
+   
+    public static void main(String[] args) {
+        System.out.println("version1");
+    }
+    
+}
